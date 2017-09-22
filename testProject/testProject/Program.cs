f1763e9e232492b8492fc4c6d77f7d8e27ef2b7c
James Platt <James.Platt@Neptune.com>
2017-09-22 13:35:46 +0100

    Added support for multiple operators

--- testProject/testProject/Program.cs
+++ testProject/testProject/Program.cs
@@ -10,7 +10,37 @@ namespace testProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("The file has been altered, pray I do not alter it further.");
+            int num1;
+            int num2;
+            Char op;
+            Double result=0;
+            Console.Write("Please enter an operator: ");
+            op = Console.ReadLine().ToCharArray()[0];
+            Console.Write("Please enter a number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter another number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                case '*':
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid Operator.");
+                    break;
+            }
+
+            Console.WriteLine("The solution is: {0}", result);
             Console.ReadLine();
         }
     }
